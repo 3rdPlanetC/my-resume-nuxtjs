8da0b3a263d8a814fe97981257c947f8e2344193
Eth Chonlasai <o_nongearth_o@hotmail.com>
2023-11-28 13:42:42 +0700

    feat : update lib version + update nuxt version + a-button component

---
+++ components/atoms/button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="a-button">
+        <slot></slot>
+    </button>
+</template>
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+.a-button {
+    cursor: pointer;
+}
+</style>
